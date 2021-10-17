2d54af06c407d84b5ea7511b2b0fc4dafeb26071
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2021-10-17 08:31:32 -0700

    Fichier setup pour le fichier exe

---
+++ setup.py
@@ -0,0 +1,19 @@
+# python setup.py build
+from cx_Freeze import setup, Executable
+  
+executables = [
+        Executable(script = "main.py",icon = "SpaceInvader/images/icone.ico", base = "Win32GUI" )
+]
+  
+buildOptions = dict( 
+        includes = ["random","time","pygame"]
+)
+  
+setup(
+    name = "Space Invader",
+    version = "1.0.4.16",
+    description = "Jeu inspiré des Space Invader développé dans le but du ProjetPrintemps en NSI",
+    author = "Louis & Fabien",
+    options = dict(build_exe = buildOptions),
+    executables = executables
+)
